a6ca049a72f253ed0d8c117246911b65c12071fc
Рябов Максим Алексеевич <m.rjabov@cft.ru>
2020-05-15 17:59:10 +0500

    Homw Work #1.2

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,64 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
 
     void clear() {
+        for (int i = 0; i <= size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        if (get(r.uuid) == null) {
+            storage[size()] = r;
+            size++;
+        } else {
+            System.out.println("Резюме с uuid = " + r.uuid + " уже существует");
+        }
     }
 
     Resume get(String uuid) {
+        int i = 0;
+        while (storage[i] != null) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+            i++;
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        boolean isFind = false;
+
+        while (i < size) {
+            if (storage[i].uuid.equals(uuid)) {
+                isFind = true;
+            }
+
+            if (isFind) storage[i] = storage[i + 1];
+
+            i++;
+        }
+        if (isFind) size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResume = new Resume[size];
+
+        for (int i = 0; i < size; i++) {
+            allResume[i] = storage[i];
+        }
+
+        return allResume;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
